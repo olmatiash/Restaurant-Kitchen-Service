1ae24947d716f436b3b0d93678d46ce66cacb95d
Alex Matiash <matiash.jr@gmail.com>
2023-09-24 10:29:09 +0300

    Add crispy_forms to istalled_apps, crispy_template_pack - bootsrap4, path to static

--- Restaurant_Kitchen_Service/settings.py
+++ Restaurant_Kitchen_Service/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crispy_forms",
     'restaurant',
 ]
 
@@ -68,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 WSGI_APPLICATION = 'Restaurant_Kitchen_Service.wsgi.application'
 
@@ -118,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
