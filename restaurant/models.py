e9d58e76af5d29341312fd711b9fd987d30632a3
Alex Matiash <matiash.jr@gmail.com>
2023-10-02 19:02:15 +0300

    Add ingredient manutomany relation to Dish

--- restaurant/models.py
+++ restaurant/models.py
@@ -28,12 +28,27 @@ class Cook(AbstractUser):
         return reverse("restaurant:cook-detail", kwargs={"pk": self.pk})
 
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    provider = models.CharField(max_length=255)
+    unit = models.CharField(max_length=10)
+    purchase_price = models.DecimalField(max_digits=7, decimal_places=2)
+
+    class Meta:
+        verbose_name = "ingredient"
+        verbose_name_plural = "ingredients"
+
+    def __str__(self):
+        return self.name
+
+
 class Dish(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
     cooks = models.ManyToManyField(Cook, related_name="dishes")
+    ingredients = models.ManyToManyField(Ingredient, related_name="dishes")
 
     class Meta:
         verbose_name = "dish"
@@ -42,17 +57,11 @@ class Dish(models.Model):
     def __str__(self):
         return self.name
 
-
-class Ingredient(models.Model):
-    name = models.CharField(max_length=255)
-    provider = models.CharField(max_length=255)
-    unit = models.CharField(max_length=10)
-    purchase_price = models.DecimalField(max_digits=7, decimal_places=2)
-    dishes = models.ManyToManyField(Dish, related_name="ingredients")
-
-    class Meta:
-        verbose_name = "ingredient"
-        verbose_name_plural = "ingredients"
-
-    def __str__(self):
-        return self.name
+    @property
+    def total_cost(self):
+        queryset = self.ingredients.all().aggregate(
+            total_cost=models.Sum(
+                "purchase_price"
+            )
+        )
+        return queryset["total_cost"]
