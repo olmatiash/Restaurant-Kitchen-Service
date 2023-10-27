8174669e65f1731a9b98bb971d5fb6c6a0dae1c4
Alex Matiash <matiash.jr@gmail.com>
2023-10-27 18:43:07 +0300

    Return SECRET_KEY setup

--- Restaurant_Kitchen_Service/settings.py
+++ Restaurant_Kitchen_Service/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-u8n99!-r-gfar%v0p=(%t9!90ip96qm-zrxp(l&&)2mpx14^i2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,6 +87,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+
     }
 }
 
